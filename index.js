6a7ba48af697d0f8ffd3a2b53b0dc22d568cf626
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-05-20 11:34:22 +0600

    jwt token verify done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 
@@ -18,6 +19,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const JWKS = createRemoteJWKSet(new URL("http://localhost:3000/api/auth/jwks"));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Forbidden" });
+  }
+
+  
+};
 
 async function run() {
   try {
@@ -39,22 +64,11 @@ async function run() {
       res.json(result);
     });
 
-    app.get(
-      "/pet/:id",
-      (req, res, nex) => {
-        const header = req.headers.authorization;
-       if(header == "logged in"){
-         nex();
-       } else{
-        res.status(401).json({message: "Unauthorized"})
-       }
-      },
-      async (req, res) => {
-        const { id } = req.params;
-        const result = await petCollection.findOne({ _id: new ObjectId(id) });
-        res.json(result);
-      },
-    );
+    app.get("/pet/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const result = await petCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
 
     app.patch("/pet/:id", async (req, res) => {
       const { id } = req.params;
